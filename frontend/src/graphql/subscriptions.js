d216dfcd7d2aab827cc4c1055f831e64b9d42c79
brianw0924 <brianw0924@gmail.com>
2022-01-08 17:01:38 +0800

    fix subscription

--- frontend/src/graphql/subscriptions.js
+++ frontend/src/graphql/subscriptions.js
@@ -1,11 +1,15 @@
 import { gql } from "@apollo/client";
 
 export const POST_CREATED_SUBSCRIPTION = gql`
-  subscription OnPostCreated {
+  subscription postCreated {
     postCreated {
       id
       title
+      content
+      number
+      location
       timestamp
+      from
     }
   }
 `;
